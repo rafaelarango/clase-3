b960739fece0a03b7689b878c79f70519c5a74b0
JUAN ARANGO <69176004+juandiego19578@users.noreply.github.com>
2021-04-08 14:45:54 -0500

    resta

---
+++ resta.js
@@ -0,0 +1,10 @@
+const numero1 = prompt("Ingresa un numero");
+const numero2 = prompt("Ingresa un segundo numero");
+
+function suma(numero1, numero2){
+
+    console.log(numero1 - numero2)
+
+}
+
+suma(numero1, numero2);
