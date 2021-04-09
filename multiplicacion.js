1db8dc78cfb16480be7e037885c0f952c904c0ed
JUAN ARANGO <69176004+juandiego19578@users.noreply.github.com>
2021-04-08 19:17:17 -0500

    nueva const

--- multiplicacion.js
+++ multiplicacion.js
@@ -1,6 +1,6 @@
 const numero1 = prompt("Ingresa un numero");
 const numero2 = prompt("Ingresa un segundo numero");
-
+const numero = 4;
 function suma(numero1, numero2){
 
     console.log(numero1 * numero2)
